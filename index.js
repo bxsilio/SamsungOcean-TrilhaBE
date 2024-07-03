998e67db01ecad99e3a4f78999461a4f2b899a41
Moisés Basílio <134879837+mvbasilios@users.noreply.github.com>
2024-07-03 14:20:05 +0000

    correção geral do código

--- index.js
+++ index.js
@@ -10,21 +10,28 @@ app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
 
-// Lista de personagens
+// Lista de Personagens
 const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 // Read All - [GET] /item
 app.get('/item', function (req, res) {
-  //Pegamos a lista e enviamos como resposta HTTP
+  // Pegamos a lista e enviamos como resposta HTTP
   res.send(lista)
 })
-// Sinaliza para o Express o uso de JSON no Body
+
+// Sinalizamos para o Express que vamos usar JSON no Body
 app.use(express.json())
 
 // Create - [POST] /item
 app.post('/item', function (req, res) {
-  console.log(req.body)
-  res.send('Create')
+  // Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+
+  // Inserimos o item no final da lista
+  lista.push(item)
+
+  // Enviamos uma mensagem de sucesso!
+  res.send('Item criado com sucesso!')
 })
 
 app.listen(3000)
