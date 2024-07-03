37b423fcc89fdc166e186a36d897569fcbc7c317
Moisés Basílio <134879837+mvbasilios@users.noreply.github.com>
2024-07-03 13:46:49 +0000

    JSON no Body e POST

--- index.js
+++ index.js
@@ -18,5 +18,13 @@ app.get('/item', function (req, res) {
   //Pegamos a lista e enviamos como resposta HTTP
   res.send(lista)
 })
+// Sinaliza para o Express o uso de JSON no Body
+app.use(express.json())
+
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+  console.log(req.body)
+  res.send('Create')
+})
 
 app.listen(3000)
